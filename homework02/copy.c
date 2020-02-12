a78481a192d01f16f7f1b49c6ac73ad8aa6beab5
James Eapen <jpe4@students.calvin.edu>
2020-02-12 00:30:00 -0500

    added copy.c intro docs

---
+++ homework02/copy.c
@@ -0,0 +1,8 @@
+/*
+ * Copy program to copy the contents of one file to another
+ * USAGE: copy source_file destination_file
+ * source file must exist and destination file must not exist
+ * @author: James Eapen (jpe4)
+ * @data: 2020 Feb 11
+ */
+
