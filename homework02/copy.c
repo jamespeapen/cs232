74d6e22d528e643b5184b84859b1c95e834bd689
James Eapen <jpe4@students.calvin.edu>
2020-02-12 00:41:03 -0500

    created main(), docs

--- homework02/copy.c
+++ homework02/copy.c
@@ -1,8 +1,20 @@
-/*
+/**
  * Copy program to copy the contents of one file to another
- * USAGE: copy source_file destination_file
- * source file must exist and destination file must not exist
+ * * USAGE: copy source_file destination_file
+ * * source file must exist and destination file must not exist
  * @author: James Eapen (jpe4)
  * @data: 2020 Feb 11
  */
 
+#include <stdio.h>
+// TODO: std lib 
+
+/** 
+ * Main function
+ * @param param argc the number of arguments
+ * @param argvp[] an array with the arguments supplied
+ */
+int main(int argc, char *argv[])
+{
+    
+}
