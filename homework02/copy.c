5e8dfc68245b9da5b86be9588bd17011f5c98252
James Eapen <jpe4@students.calvin.edu>
2020-02-13 16:28:06 -0500

    sending all errors through stderr

--- homework02/copy.c
+++ homework02/copy.c
@@ -39,23 +39,21 @@ int main(int argc, char *argv[])
 	// usage errors
 	if (argc < 2)
 	{
-		printf("%d\n", argc);
-		printf("%s\n", "missing parameters");
-		printf("%s\n", "USAGE: copy source_file destination_file");
+		fprintf(stderr, "%s\n", "missing parameters");
+		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
 		exit(1);
 	}
 	else if (argc < 2)
 	{
-		printf("%d\n", argc);
-		printf("%s\n", "missing destination filename");
-		printf("%s\n", "USAGE: copy source_file destination_file");
+		fprintf(stderr, "%s\n", "missing destination filename");
+		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
 		exit(1);
 	}
 
 	else if (argc > 3)
 	{
-		printf("%s\n", "too many arguments");
-		printf("%s\n", "USAGE: copy source_file destination_file");
+		fprintf(stderr, "%s\n", "too many arguments");
+		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
 		exit(1);
 	}
 	
@@ -65,16 +63,16 @@ int main(int argc, char *argv[])
 	//check if source file exists
 	if (!fileExists(src))
 	{
-		printf("%s", src);
-		printf("%s\n", ": file not found");
+		fprintf(stderr, "%s", src);
+		fprintf(stderr, "%s\n", ": file not found");
 		exit(1);
 	}
 
 	// check if dest file already exists
 	if (fileExists(dest))
 	{
-		printf("%s", dest);
-		printf("%s\n", ": file already exists");
+		fprintf(stderr, "%s", dest);
+		fprintf(stderr, "%s\n", ": file already exists");
 		exit(1);
 	}
 
