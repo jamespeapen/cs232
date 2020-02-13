f6dc385883ac230da21695842366704d773b4bb6
James Eapen <jpe4@students.calvin.edu>
2020-02-13 16:35:48 -0500

    changed fprintf to perror

--- homework02/copy.c
+++ homework02/copy.c
@@ -39,21 +39,21 @@ int main(int argc, char *argv[])
 	// usage errors
 	if (argc < 2)
 	{
-		fprintf(stderr, "%s\n", "missing parameters");
-		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
+		perror("missing parameters");
+		perror("USAGE: copy source_file destination_file");
 		exit(1);
 	}
 	else if (argc < 2)
 	{
-		fprintf(stderr, "%s\n", "missing destination filename");
-		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
+		perror("missing destination filename");
+		perror("USAGE: copy source_file destination_file");
 		exit(1);
 	}
 
 	else if (argc > 3)
 	{
-		fprintf(stderr, "%s\n", "too many arguments");
-		fprintf(stderr, "%s\n", "USAGE: copy source_file destination_file");
+		perror("too many arguments");
+		perror("USAGE: copy source_file destination_file");
 		exit(1);
 	}
 	
@@ -63,16 +63,16 @@ int main(int argc, char *argv[])
 	//check if source file exists
 	if (!fileExists(src))
 	{
-		fprintf(stderr, "%s", src);
-		fprintf(stderr, "%s\n", ": file not found");
+		perror(src);
+		perror(": file not found");
 		exit(1);
 	}
 
 	// check if dest file already exists
 	if (fileExists(dest))
 	{
-		fprintf(stderr, "%s", dest);
-		fprintf(stderr, "%s\n", ": file already exists");
+		perror(dest);
+		perror(": file already exists");
 		exit(1);
 	}
 
