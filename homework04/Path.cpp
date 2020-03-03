36bd4e84aff3d80829628268152c8902e01f2be3
James Eapen <jpe4@students.calvin.edu>
2020-03-03 16:57:06 -0500

    added inital definitions for functions

---
+++ homework04/Path.cpp
@@ -0,0 +1,28 @@
+/* Path.h declares a path class for MEShell to represent the directories in which it searches for executable programs
+ * James Eapen (jpe4)
+ * Date: 2020 March 9
+ */
+
+#include "Path.h"
+#include <string>
+#include <cstdlib>
+
+/* Constructor
+ */
+Path::Path() {
+
+}
+
+/* return the index of the directory containing the program
+ */ 
+int Path::find(const std::string& program) {
+	return -1;
+}
+
+/* return the name of the directory whose index is i
+ */
+
+std::string Path::getDirectory(int i) const{
+	return "string";
+}
+ 
