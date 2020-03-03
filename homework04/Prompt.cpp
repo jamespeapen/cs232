bf3cc2e208b75fa84d2442f1de014824a68715e7
James Eapen <jpe4@students.calvin.edu>
2020-03-03 17:05:21 -0500

    added starter definitions for prompt functions

---
+++ homework04/Prompt.cpp
@@ -0,0 +1,19 @@
+/* Function definitions for the MEShell command line prompt
+ *  James Eapen (jpe4)
+ *  Date: 2020 March 9
+ */ 
+
+#include "Prompt.h"
+#include <string>
+
+/*Constructor that builds a prompt as the full path to the working directory
+ */
+Prompt::Prompt() {
+	//TODO create constructor
+}
+
+/* an accessor that returns the current value of the prompt 
+ */
+string Prompt::get() const {
+	//TODO create function
+}
