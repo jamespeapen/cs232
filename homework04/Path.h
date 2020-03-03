8f6417df84088fae1b3821ab13123ef155b1abc2
James Eapen <jpe4@students.calvin.edu>
2020-03-03 15:32:51 -0500

    created path class with constructor, find and getDirectory

---
+++ homework04/Path.h
@@ -0,0 +1,15 @@
+/* Path.h declares a path class for MEShell to represent the directories in which it searches for executable programs
+ * James Eapen (jpe4)
+ * Date: 2020 March 9
+ */
+
+#ifndef PATH_H_
+#define PATH_H_
+#include <string>
+
+class Path {
+	public:
+		Path();
+		int find(const std::string& program);
+		std::string getDirectory(int i) const;
+};
