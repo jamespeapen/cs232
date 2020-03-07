3ef087299fb0294bdf81a92e4dc657bea7c4b3f1
vmartinezc <vamc98@hotmail.com>
2020-03-07 13:32:42 -0500

    CommandLine class

---
+++ homework04/ComandLine.cpp
@@ -0,0 +1,25 @@
+/*
+
+   CommandLine.cpp
+
+   author: Valeria Martinez (@vam6)
+   partner: James Eapen (@jpe4)
+   class: CS232, Spring 2020
+   date: 03/07/2020
+
+ */
+
+
+#include "CommandLine.h"
+
+CommandLine::CommandLine(istream& in){
+
+    ampersand = false;
+
+
+}
+
+
+
+
+
