7e11d50287761c689b52eca5bfc7d764139519f1
James Eapen <jpe4@students.calvin.edu>
2020-03-10 15:56:14 -0400

    initial header file for MEShell class

--- homework04/MEShell.h
+++ homework04/MEShell.h
@@ -1,12 +1,20 @@
-/* MEShell.h declares a driver class for MEShell
+/* MEShell.h declares a class for the MEShell environment
  */
 
 #ifndef MESHELL_H_
 #define MESHELL_H_ 
+#include "Path.h"
+#include "Prompt.h"
 #include <string>
-
 class MEShell {
 
+	public:
+		MEShell();
+		void run();
+	
+	private:
+		std::string prompt;
+		std::string path;
 };
 
 #endif /* MESHELL_H_  */
