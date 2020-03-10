ef368a909966454d0702858a87f19ec5bcdf92d7
James Eapen <jpe4@students.calvin.edu>
2020-03-10 15:47:37 -0400

    moved MEShell driver code to main

---
+++ homework04/main.cpp
@@ -0,0 +1,21 @@
+/* Driver for the MEShell
+ * James Eapen (jpe4)
+ * Valeria Martinez (vam6)
+ */
+
+#include "MEShell.h"
+#include "Path.h"
+#include "Prompt.h"
+#include <iostream>
+#include <string>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "MEShell" << endl;
+	std::string test_input;	
+	Path path = Path();
+	//cout << path.get_directory(path.find("cp"));
+	Prompt prompt = Prompt();
+	cout << prompt.get();
+}
