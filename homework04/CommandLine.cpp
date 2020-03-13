588ddbb359a713214468368fea7beac13e4b5241
James Eapen <jpe4@students.calvin.edu>
2020-03-13 16:25:53 -0400

    fixed argv

--- homework04/CommandLine.cpp
+++ homework04/CommandLine.cpp
@@ -18,34 +18,36 @@
 
 
 CommandLine::CommandLine(istream& in){
-    
-    ampersand = false;
-//    cout << "Waiting for CLI input" << endl;
-    getline(in, myCommandLine);
-//    cout << "Your input was " << myCommandLine << endl;
-    istringstream iss  (myCommandLine);
-    string aTempString;
-    vector<char*> charVec = vector<char*>();
 
-//push istringstream converted strings from commandline into the vector
-    for (string s; iss >> s; ) {
+    	ampersand = false;
+    	getline(in, myCommandLine);
+    	istringstream iss  (myCommandLine);
+
+    	string aTempString;
+
+	//check if command has ampersand
+	
+	for (string s; iss >> s;) {
 		commandLineWords.push_back(s);
 		my_argc += 1;
 	}
 
-//check if command has ampersand
-    while(iss >> aTempString){
-        commandLineWords.push_back(aTempString);
-        if (strcmp(aTempString.c_str(), "&") != 0) { //if commmand has an ampersand, return 
-		ampersand = true;
+	while(iss >> aTempString){
+	        if (strcmp(aTempString.c_str(), "&") == 0) { 
+			//if commmand has an ampersand, return 
+			ampersand = true;
+		}
+		else {
+			commandLineWords.push_back(aTempString);
+		}
 	}
 
-    for(int i = 0; i < commandLineWords.size(); i++){
-        charVec.push_back((char*)commandLineWords[i].data());
-    }
-    charVec.push_back(NULL);
-    my_argv = charVec.data();
+	my_argv = new char* [commandLineWords.size()];
+
+	for (int i = 0; i < commandLineWords.size(); i++) {
+		my_argv[i] = new char[commandLineWords[i].size() + 1];
+		strcpy(my_argv[i], commandLineWords[i].c_str());
+	}
 }
-    }
 
 
