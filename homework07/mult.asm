656f869be62b626b1fa7dc841406d7351293497a
James Eapen <jpe4@students.calvin.edu>
2020-04-28 01:39:26 -0400

    fixed 12 to 14

--- homework07/mult.asm
+++ homework07/mult.asm
@@ -1,7 +1,7 @@
 # Multiply two numbers, found in locations 32 and 33, leaving the result in location 34.
 # Assumes this code is loaded at location 20.
 __main: 0
-mov 0 12
+mov 0 14
 mov *12 reg2
 jez reg2 11
 mov *13 reg1
@@ -11,6 +11,6 @@ sub 1 reg1
 jez reg1 10
 add reg0 reg2
 jmp 6
-mov reg2 12
+mov reg2 14
 end
 __data: 3
