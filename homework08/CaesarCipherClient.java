122178d44e3d3353495c6acbebdd76345229af02
James Eapen <jpe4@students.calvin.edu>
2020-04-30 21:30:36 -0400

    Client and server inits

---
+++ homework08/CaesarCipherClient.java
@@ -0,0 +1,10 @@
+/* Homework 8: Caesar Cipher Client
+ * James Eapen (jpe4)
+ * 2020 May 9
+ *
+ * Connects to a Caesar cipher server sending it a rotation.
+ * It receives user input, sends it to the 
+ * server, and receives and displays the server response
+ */
+
+
---
+++ homework08/CaesarCipherServer.java
@@ -0,0 +1,9 @@
+/* Homework 8: Caesar Server 
+ * James Eapen (jpe4)
+ * 2020 May 9
+ *
+ * Opens a TCP socket and listens for client connections
+ * Receives an int n and saves and returns it
+ * Spawns new threads to handle clientsj
+ *
+ */
