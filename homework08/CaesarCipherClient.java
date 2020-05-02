620839d15fbe5f7c7da7ba7ecbae5f6e258590cd
James Eapen <jpe4@students.calvin.edu>
2020-05-01 20:54:20 -0400

    imports and welcome

--- homework08/CaesarCipherClient.java
+++ homework08/CaesarCipherClient.java
@@ -7,4 +7,24 @@
  * server, and receives and displays the server response
  */
 
+import java.net.Socket;
+import java.net.SocketException;
 
+import sun.awt.image.InputStreamImageSource;
+
+import java.io.DataOutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class CaesarClient {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Caesar Ciper Client");
+        System.out.println("Enter the rotation amount: ");
+        int rotation;
+        
+    }
+
+}
