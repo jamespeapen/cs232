e944017453128c8c436e1ed92b51e4caa77acba2
James Eapen <jpe4@students.calvin.edu>
2020-05-06 16:13:58 -0400

    connecting and returning ciphered text; not quitting

--- homework08/CaesarCipherClient.java
+++ homework08/CaesarCipherClient.java
@@ -14,6 +14,7 @@ import java.net.UnknownHostException;
 import java.io.DataOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.io.IOException;
 
 
@@ -36,23 +37,26 @@ public class CaesarCipherClient {
 
         try (
             Socket CaesarScocket = new Socket(hostname, port);
-            DataOutputStream dataOut = new DataOutputStream(CaesarScocket.getOutputStream());
+            PrintWriter dataOut = new PrintWriter(CaesarScocket.getOutputStream(), true);
             BufferedReader dataIn = new BufferedReader(new InputStreamReader(CaesarScocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             )
         {
-            System.out.println("Connecting to " + hostname + " on port " + port);
-            dataOut.writeBytes(Integer.toString(rotation));
-            dataOut.flush();
-            System.out.println("sent rotation " + rotation);
-            String line;
-            System.out.println("reading");
-            System.out.println(dataIn.readLine());
-            while(dataIn.readLine() != null){
-                System.out.println("getting input");
-                line = dataIn.readLine();
-                System.out.println(line);
+              System.out.println("Connecting to " + hostname + " on port " + port);
+              dataOut.println(String.valueOf(rotation));
+              System.out.println(dataIn.readLine());
+                
+              String line;
+              line = stdIn.readLine();
+              while ((line = stdIn.readLine()) != null) {
+                  System.out.println("line: " + line);
+                  if (line == "quit") {
+                      System.exit(0);
+                  }
+                  dataOut.println(line);
+                  System.out.println("Server: " + dataIn.readLine());
+              }
             }
-        }
         catch(UnknownHostException e) {
             System.out.println("Error: " + hostname + " not known");
             System.exit(1);
