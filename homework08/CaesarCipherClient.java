0242d57dede3c07dc543c72a8975f350c963f494
James Eapen <jpe4@students.calvin.edu>
2020-05-09 23:41:52 -0400

    docs

--- homework08/CaesarCipherClient.java
+++ homework08/CaesarCipherClient.java
@@ -2,7 +2,7 @@
  * James Eapen (jpe4)
  * 2020 May 9
  *
- * Connects to a Caesar cipher server sending it a rotation.
+ * This client connects to a Caesar cipher server sending it a rotation.
  * It receives user input, sends it to the 
  * server, and receives and displays the server response
  * Citation: https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoClient.java
